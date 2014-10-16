a393e588b2cae4ea3dfb08f1a088d87f82127065
MickeMakaron <micke_makaron@hotmail.com>
2014-10-16 16:17:37 +0200

    Copyright notice

--- gyg/index.php
+++ gyg/index.php
@@ -1,8 +1,32 @@
 <?php
-// Hobo-fix for disabling index browsing without .htaccess.
-// An index.php file should exist in ALL directories.
+/****************************************************************
+ ****************************************************************
+ * 
+ * gyg-framework - Basic framework for web development
+ * Copyright (C) 2014 Mikael Hernvall (mikael.hernvall@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************
+ ****************************************************************/
 
-// Do the exact same thing as gyg's standard helper function "throw404".
+
+/*
+ * Hobo-fix for disabling index browsing without .htaccess.
+ * An index.php file should exist in ALL directories.
+ * Do the exact same thing as gyg's standard helper function "throw404".
+ */
 header('HTTP/1.0 404 Not Found');
 include($_SERVER['DOCUMENT_ROOT'] . "/gyg/404.php");
 die();
